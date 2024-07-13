3993233ac29ee3db292ffcec59ff0b2ce72ba204
mohfauzislamet <mohfauzislamet@gmail.com>
2024-07-13 22:19:44 +0700

    Doc : (Router) documentation

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -9,6 +9,10 @@ import 'package:todo_apps/config/routes/route_name.dart';
 
 part 'app_router.gr.dart';
 
+/// replaceInRouteName: Menentukan string yang akan diganti dalam nama rute.
+/// Dalam hal ini, Route dan Page akan diganti dengan Route.
+///
+/// generateForDir: Menentukan direktori mana yang akan di-scan untuk menemukan definisi rute.
 @AutoRouterConfig(
   replaceInRouteName: "Route|Page,Route",
   generateForDir: [
@@ -16,8 +20,16 @@ part 'app_router.gr.dart';
     'lib/app/features',
   ],
 )
+
+/// Dengan menggunakan @lazySingleton, aplikasi dapat mengelola dan menyediakan
+/// objek AppRouter secara efisien, dengan memastikan hanya ada satu instance yang
+/// digunakan di seluruh aplikasi, dan diinisialisasi secara lazy (hanya saat diperlukan).
+/// Ini mendukung praktik-praktik desain yang baik dalam manajemen ketergantungan dan
+/// penggunaan sumber daya dalam aplikasi Flutter.
 @lazySingleton
 class AppRouter extends _$AppRouter {
+  /// Menentukan jenis rute default yang akan digunakan oleh aplikasi.
+  /// Dalam kasus ini, rute default adalah RouteType.material.
   @override
   RouteType get defaultRouteType => const RouteType.material();
 
