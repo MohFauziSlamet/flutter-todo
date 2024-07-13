0e3e058111a820df11fbc4bdba550b5396e92dd5
mohfauzislamet <mohfauzislamet@gmail.com>
2024-07-13 22:38:22 +0700

    Doc : DI documentation

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -2,8 +2,15 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:todo_apps/core/di/injection.config.dart';
 
+/// `GetIt.instance` digunakan untuk mendapatkan instance tunggal dari GetIt,
+/// yang merupakan container dependency injection.
 final getIt = GetIt.instance;
 
+/// @InjectableInit digunakan untuk menandai fungsi atau metode yang akan menginisialisasi dependency injection.
+/// initializerName: Nama dari fungsi inisialisasi yang akan digenerate (dalam kasus ini, $initGetIt).
+/// preferRelativeImports: Menunjukkan apakah import yang dihasilkan akan menggunakan path relatif atau absolut.
+/// asExtension: Menentukan apakah kode yang dihasilkan akan menggunakan ekstensi (extension) atau tidak.
+/// get_context_func: Daftar direktori yang akan dipindai untuk mencari kelas-kelas yang dianotasi dengan @injectable.
 @InjectableInit(
   initializerName: r'$initGetIt',
   preferRelativeImports: false,
--- lib/core/di/module/injection_module.dart
+++ lib/core/di/module/injection_module.dart
@@ -2,10 +2,20 @@ import 'package:injectable/injectable.dart';
 import 'package:todo_apps/config/source_config/local/hive_config.dart';
 import 'package:todo_apps/config/source_config/local/hive_config_impl.dart';
 
+/// `@module` adalah anotasi yang digunakan untuk menandai kelas
+/// sebagai modul konfigurasi dependency injection. Ini berarti kelas
+/// ini akan berisi definisi-definisi untuk objek-objek yang akan
+/// di-inject (disuntikkan) ke dalam aplikasi.
 @module
 abstract class InjectionModules {
+  /// `@lazySingleton` adalah anotasi yang digunakan untuk menandai bahwa
+  /// objek yang di-injek akan berupa singleton (hanya ada satu instance di seluruh aplikasi),
+  /// dan akan diinisialisasi secara malas (lazy initialization), yaitu hanya saat pertama kali diminta.
+  ///
+  /// `@preResolve` digunakan bersama dengan @lazySingleton untuk menandai bahwa
+  /// objek yang di-injek akan diinisialisasi sebelum aplikasi benar-benar membutuhkannya (pre-resolve).
+  /// Hal ini membantu dalam memastikan bahwa semua dependensi sudah siap saat aplikasi dimulai.
   @lazySingleton
   @preResolve
   Future<HiveConfig> boxClient() => HiveConfigImpl.instance();
-
 }
